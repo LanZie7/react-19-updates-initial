c04e10a0d12cd0fe48af7595978d2f0284463531
bochkova_su <bochkova_su@resanta.ru>
2024-12-26 19:53:41 +0300

    use added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import OptimisticMessage from './components/OptimisticMessage'
+import UseExample from './components/UseExample'
 
 export default function App() {
   return (
     <div className="container">
       <h1>React 19 – Overview</h1>
-      <OptimisticMessage />
+      <UseExample />
     </div>
   )
 }
